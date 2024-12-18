431906a0eecdcd6d33efd89a75c70a6566b4d9b7
devaryakjha <dev.jha.arya@gmail.com>
2024-12-19 05:04:40 +0530

    fix: checkout to head branch wasn't working properly

--- packages/tagflow/lib/tagflow.dart
+++ packages/tagflow/lib/tagflow.dart
@@ -1,17 +1,12 @@
-// core -> models -> element.dart
 export 'src/converter/converter.dart';
 export 'src/converter/converters/converters.dart';
 export 'src/converter/styled_container.dart';
 export 'src/core/core.dart';
 export 'src/core/models/models.dart';
-// Styles
 export 'src/style/style.dart';
 export 'src/style/style_parser.dart';
 export 'src/style/theme.dart';
 export 'src/style/types.dart';
-// Options
 export 'src/tagflow_options.dart';
-// Main entry point for rendering HTML content
 export 'src/tagflow_widget.dart';
-// Widgets
 export 'src/widgets/widgets.dart';
