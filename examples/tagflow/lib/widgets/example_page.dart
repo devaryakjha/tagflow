8e1d1ec1575dbcc6331ddc50f9b6d03a85552518
devaryakjha <dev.jha.arya@gmail.com>
2026-06-11 10:08:46 +0530

    fix(ci): validate alpha workspace tests

--- examples/tagflow/lib/widgets/example_page.dart
+++ examples/tagflow/lib/widgets/example_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tagflow/tagflow.dart';
+import 'package:tagflow/legacy.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 final _key = GlobalKey<_ExamplePageState>();
